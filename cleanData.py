8f40754e66947a9210b6bd96ba0d2b3e1f32c7fc
mary0408-1736691 <mary0408@uw.edu>
2020-08-20 23:53:25 +0800

    Add correlation

--- cleanData.py
+++ cleanData.py
@@ -2,55 +2,53 @@ import pandas as pd
 import geopandas as gpd
 import plotly.express as px
 import matplotlib.pyplot as plt
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go
-
-
-# import dataset 
-geo_df = gpd.read_file('data/geo-data/ne_110m_admin_0_countries.shp')
-data_2019 = pd.read_csv('data/datasets-894-813759-2019.csv')
-data_2019 = pd.read_csv('data/datasets-894-813759-2019.csv')
-gdp = pd.read_csv('data/gdp.csv')
-
+import numpy as np
 
  # Part1 Clean Data
-
 #refine data according to the prompt (set United States as United States of America)
-column = data_2019.columns
-    
-data_2019.iloc[18, column.get_loc('Country or region')] = 'United States of America'
+def clean_task1(data_2019, geo_df):
+    column = data_2019.columns
+    row = data_2019[data_2019['Country or region'] == 'United States'].index[0]
 
-    
-country_score = data_2019[['Country or region', 'Score']]
-country_geometry = geo_df[['SOVEREIGNT', 'geometry']]
-geo_merge = country_geometry.merge(country_score, left_on='SOVEREIGNT', right_on='Country or region', how='left')
-
-geo_filtered = geo_merge[geo_merge['Score'].notnull()]
-
-fig, ax = plt.subplots(1)
-geo_merge.plot(color= '#CCCCCC', ax=ax)
-geo_filtered.plot(column='Score', cmap='OrRd', ax=ax, legend=True)
-
-plt.title('Happiness Score Map')
-plt.show()
-
+    data_2019.iloc[row, column.get_loc('Country or region')] = \
+                  'United States of America'
+    country_score = data_2019[['Country or region', 'Score']]
+    country_geometry = geo_df[['SOVEREIGNT', 'geometry']]
+    geo_merge = country_geometry.merge(country_score, left_on='SOVEREIGNT', 
+    right_on='Country or region', how='left')
+    return geo_merge
 
 
 # Part2 Clean Data
-top20_happiness = data_2019[['Country or region', 'Score']].head(20)
-gdp_2019 = gdp[['Country Name', '2019']]
-gdp_merged = top20_happiness.merge(gdp_2019, left_on='Country or region',
+def clean_task2(data_2019, gdp_file):
+    top20_happiness = data_2019[['Country or region', 'Score']].head(20)
+    gdp_2019 = gdp_file[['Country Name', '2019']]
+    gdp_merged = top20_happiness.merge(gdp_2019, left_on='Country or region',
                                    right_on='Country Name', how='left')
+    return gdp_merged
 
-# Plot gdp-happiness
-fig = px.scatter(gdp_merged, x='Country Name', y='2019', color='Score',
-                 size='Score', log_y=True,
-                 labels={
-                    'Country Name': 'Countries',
-                    '2019': 'GDP in 2019'})
-fig.update_layout(title={
-                    'text': "Top20 Happiest Countries With Their GDP",
-                    'y':0.96,
-                    'x':0.5,
-                    'xanchor': 'center',
-                    'yanchor': 'top'})
+
+
+# Part3 Clean Data
+def clean_task3(data_2019):
+    extract = data_2019[['Score', 'GDP per capita', 
+                        'Social support', 'Healthy life expectancy',
+                        'Freedom to make life choices', 'Generosity', 
+                        'Perceptions of corruption']]
+    col = extract.columns
+    for name in col:
+        extract=extract[extract[name].notnull()]
+    
+    score = extract[['Score']]
+    score_li = np.array(score)
+    construct = score_li.reshape(1, len(score))
+
+    for column in col:
+        if column != "Score":
+            element = extract[[column]]
+            column = np.array(element)
+            reshape = column.reshape(1, len(element))
+            construct = np.append(construct, reshape, axis=0)
+    correlation = np.corrcoef(construct)
+    return correlation
+
--- main.py
+++ main.py
@@ -1,1 +1,60 @@
-cse 163 mian.py
+
+import pandas as pd
+import geopandas as gpd
+import plotly.express as px
+import matplotlib.pyplot as plt
+import numpy as np
+
+import cleanData
+
+
+def plot_task1(data_2019, geo_df):
+
+    geo_merge = cleanData.clean_task1(data_2019, geo_df)
+    geo_filtered = geo_merge[geo_merge['Score'].notnull()]
+
+    fig, ax = plt.subplots(1)
+    geo_merge.plot(color= '#CCCCCC', ax=ax)
+    geo_filtered.plot(column='Score', cmap='OrRd', ax=ax, legend=True)
+
+    plt.title('Happiness Score Map')
+    plt.savefig('Happiness Score map.png')
+    plt.show()
+
+
+
+def plot_task2(data_2019, gdp):
+    gdp_merged = cleanData.clean_task2(data_2019, gdp)
+    fig = px.scatter(gdp_merged, x='Country Name', y='2019', color='Score',
+                 size='Score', log_y=True,
+                 labels={
+                    'Country Name': 'Countries',
+                    '2019': 'GDP in 2019'})
+    fig.update_layout(title={
+                    'text': "Top20 Happiest Countries With Their GDP",
+                    'y':0.96,
+                    'x':0.5,
+                    'xanchor': 'center',
+                    'yanchor': 'top'})
+
+    fig.show()
+
+
+def analyze_task3(data_2019):
+    nump = cleanData.clean_task3(data_2019)
+    print(nump)
+
+
+
+def main():
+    geo_df = gpd.read_file('data/geo-data/ne_110m_admin_0_countries.shp')
+    data_2019 = pd.read_csv('data/datasets-894-813759-2019.csv')
+    gdp = pd.read_csv('data/gdp.csv')
+
+    #plot_task1(data_2019, geo_df)
+    #plot_task2(data_2019, gdp)
+    analyze_task3(data_2019)
+
+
+if  __name__ == '__main__':
+    main()

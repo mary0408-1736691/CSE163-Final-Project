33a310890d3c583d61ea318040ce45e3b96afc41
VickyChen108 <ziqic2@uw.edu>
2020-08-21 15:13:32 +0800

    flake8 change

--- cleanData.py
+++ cleanData.py
@@ -3,15 +3,15 @@ import numpy as np
 # Part1 Clean Data
 # refine data according to the prompt (set United States as United States of
 # America)
+
+
 def clean_map_data(data_2019, geo_df):
     column = data_2019.columns
     row = data_2019[data_2019['Country or region'] == 'United States'].index[0]
-
-    data_2019.iloc[row, column.get_loc('Country or region')] = \
-                  'United States of America'
+    data_2019.iloc[row, column.get_loc('Country or region')] = 'United States of America'
     country_score = data_2019[['Country or region', 'Score']]
     country_geometry = geo_df[['SOVEREIGNT', 'geometry']]
-    geo_merge = country_geometry.merge(country_score, left_on='SOVEREIGNT', 
+    geo_merge = country_geometry.merge(country_score, left_on='SOVEREIGNT',
     right_on='Country or region', how='left')
     return geo_merge
 
@@ -25,17 +25,15 @@ def clean_gdp_data(data_2019, gdp_file):
     return gdp_merged
 
 
-
 # Part3 Clean Data
 def clean_correlation_data(data_2019):
-    extract = data_2019[['Score', 'GDP per capita', 
-                        'Social support', 'Healthy life expectancy',
-                        'Freedom to make life choices', 'Generosity', 
-                        'Perceptions of corruption']]
+    extract = data_2019[['Score', 'GDP per capita',
+                         'Social support', 'Healthy life expectancy',
+                         'Freedom to make life choices', 'Generosity',
+                         'Perceptions of corruption']]
     col = extract.columns
     for name in col:
-        extract=extract[extract[name].notnull()]
-    
+        extract = extract[extract[name].notnull()]
     score = extract[['Score']]
     score_li = np.array(score)
     construct = score_li.reshape(1, len(score))
